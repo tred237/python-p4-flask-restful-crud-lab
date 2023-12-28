973c75250a300dd7d53712c40653a36409f9628f
trevor dewindt <trevor.dewindt@gmail.com>
2023-12-28 15:52:41 -0500

    add final code

--- server/app.py
+++ server/app.py
@@ -40,13 +40,32 @@ class Plants(Resource):
 
 api.add_resource(Plants, '/plants')
 
-
+import ipdb
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        for attribute in request.get_json():
+            setattr(plant, attribute, request.get_json()[attribute])
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(
+            plant.to_dict(),
+            200
+        )
 
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response(
+            '',
+            204
+        )
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
